5348b137d25044b6b253833e58e34a4530d80cf6
Dean Geckt <t-deangeckt@microsoft.com>
2023-06-29 11:43:14 +0300

    fix names

--- app/pages/2_single_dialog.py
+++ app/pages/2_single_dialog.py
@@ -6,17 +6,17 @@ import os
 import datetime
 
 root_folder = r"data/prolific/"
-version_details = {'2.1.0_0_p': 'Rule Based navigator Bot',
+version_details = {'2.1.0_0_p': 'Rule Based navigator Bot - experiment #1',
                    '2.1.0_p': 'GPT based navigator bot - experiment #1. the human had 5 minutes timer',
-                   '2.1.1_p': 'GPT based navigator bot - experiment #1. the human had 7 minutes timer',
+                   '2.1.1_p': 'GPT based navigator bot - experiment #2. the human had 7 minutes timer',
                    '2.2.2_p': 'GPT based instructor bot - experiment #1. the human had 7 minutes timer',
                    '2.2.3_p': 'GPT based instructor bot - experiment #2. step by step prompt. the human had 7 minutes timer',
                    }
-experiments_short_names = {'2.1.0_0_p': 'rb navigator',
-                           '2.1.0_p': 'GPT navigator, 5 - #1',
-                           '2.1.1_p': 'GPT navigator, 7- #1',
-                           '2.2.2_p': 'GPT instructor, 7 - #1',
-                           '2.2.3_p': 'GPT instructor, 7 - #2',
+experiments_short_names = {'2.1.0_0_p': 'RB navigator #1',
+                           '2.1.0_p': 'GPT navigator #1',
+                           '2.1.1_p': 'GPT navigator #2',
+                           '2.2.2_p': 'GPT instructor #1',
+                           '2.2.3_p': 'GPT instructor #2',
                            }
 def read_raw_data():
     data_list = []
