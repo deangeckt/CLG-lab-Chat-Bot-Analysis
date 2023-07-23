08e2eaf15565834eedde19c8a5b464a09cef70a8
Dean Geckt <t-deangeckt@microsoft.com>
2023-07-23 21:09:40 +0300

    2.2.4 data still in tmp

--- app/pages/common/versions.py
+++ app/pages/common/versions.py
@@ -3,18 +3,21 @@ version_details = {'2.1.0_0_p': 'Rule Based navigator Bot - experiment #1',
                    '2.1.1_p': 'GPT based navigator bot - experiment #2. the human had 7 minutes timer',
                    '2.2.2_p': 'GPT based instructor bot - experiment #1. the human had 7 minutes timer',
                    '2.2.3_p': 'GPT based instructor bot - experiment #2. step by step prompt. the human had 7 minutes timer',
+                   '2.2.4_p': 'Ins,Nav,Ins,Nav - 4 maps. English new instructions' # +retry mechanism when calling API
                    }
 experiments_short_names = {'2.1.0_0_p': 'RB navigator #1',
                            '2.1.0_p': 'GPT navigator #1',
                            '2.1.1_p': 'GPT navigator #2',
                            '2.2.2_p': 'GPT instructor #1',
                            '2.2.3_p': 'GPT instructor #2',
+                           '2.2.4_p': 'Alternation #1'
                            }
 time_success_metric = {'2.1.0_0_p': 300,
                        '2.1.0_p': 300,
                        '2.1.1_p': 420,
                        '2.2.2_p': 420,
-                       '2.2.3_p': 420
+                       '2.2.3_p': 420,
+                       '2.2.4_p': 420
                        }
 
-root_folder = r"data/prolific/"
+root_folder = r"data/tmp/"
