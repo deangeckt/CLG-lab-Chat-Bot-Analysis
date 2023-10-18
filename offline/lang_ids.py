64bffd07beedbe1dd570f064ef4b97183baae28a
Dean Geckt <t-deangeckt@microsoft.com>
2023-10-18 11:38:24 +0300

    lang ids - order & comparison & bert model

--- offline/lang_ids.py
+++ offline/lang_ids.py
@@ -1,27 +1,54 @@
-import numpy as np
-import pandas as pd
 import json
 import os
 from lingua import Language, LanguageDetectorBuilder
-from collections import defaultdict
-import itertools
-
-from tqdm import tqdm
-
-root_folder = r"../data/prolific/"
-output_folder = r'../data/prolific_lang_ids'
-global_cs_counter=0
-
-def pred_lang_token(token: str):
-    confidence_values  = detector.compute_language_confidence_values(token)
-    simple_list = [(v.value, v.language) for v in confidence_values]
-    conf, lng = max(simple_list)
-    return conf, lng
+import langid
+from codeswitch.codeswitch import LanguageIdentification
 
 
+def pred_token_lingua(token: str):
+    lang = lingua_detector.detect_language_of(token)
+    return 'eng' if lang == Language.ENGLISH else 'es'
 
-def run_on_dataset():
-    global global_cs_counter
+
+def pred_token_langid(text):
+    res = langid.classify(text)
+    return 'eng' if res[0] == 'en' else 'es'
+
+
+def cs_clf_heuristic(langs: list):
+    if 'eng' in langs and 'es' in langs:
+        return 'mix'
+    if 'eng' in langs:
+        return 'eng'
+    if 'es' in langs:
+        return 'es'
+    return 'both'
+
+
+def pred_sentence_via_token_lvl(sentence: str, pred_token: callable):
+    langs = [pred_token(token) for token in sentence.split(' ')]
+    return cs_clf_heuristic(langs)
+
+
+def pred_sentence_bert(sentence: str):
+    # https://huggingface.co/sagorsarker/codeswitch-spaeng-lid-lince
+    result = lid.identify(sentence)
+    langs = ['eng' if r['entity'] == 'en' else 'es' for r in result]
+    return cs_clf_heuristic(langs)
+
+
+def pred_sentence_lingua(sentence: str):
+    result = lingua_detector.detect_multiple_languages_of(sentence)
+    if len(result) == 1:
+        return 'eng' if result[0].language == Language.ENGLISH else 'es'
+    else:
+        return 'mix'
+
+
+def clf_map_task_dataset():
+    root_folder = r"../data/prolific/"
+    output_folder = r'../data/prolific_lang_ids'
+
     for file_name in os.listdir(root_folder):
         json_file = open(os.path.join(root_folder, file_name), encoding='utf8')
 
@@ -34,164 +61,63 @@ def run_on_dataset():
             chat = game['chat']
             for chat_ele in chat:
                 uter = chat_ele['msg']
-                uter_lng = custom_pred_lang_sentence(uter)
-
-                tokens = uter.split(' ')
-
-                words = []
-                for token in tokens:
-                    conf, lang = pred_lang_token(token)
-                    lang = 'eng' if lang == Language.ENGLISH else 'es'
-                    words.append({'token': token, 'lang': lang, 'conf': conf})
-
-                chat_ele['words'] = words
+                uter_lng = pred_sentence_bert(uter)
                 chat_ele['lang'] = uter_lng
 
-                # if uter_lng == 'mix':
-                #     print(f'__{uter}__')
-                #     global_cs_counter = global_cs_counter + 1
-                # if uter_lng == 'en' and len(tokens) == 1:
-                #     print(f'__{uter}__')
-                #     global_cs_counter = global_cs_counter + 1
-
-        with open(os.path.join(output_folder, file_name), 'w') as f:
-            json.dump(data, f)
-
-def print_deubg(text: str):
-    print(text, pred_lang_token(text))
-    for token in text.split(' '):
-        print(token, pred_lang_token(token))
-    print()
+        # with open(os.path.join(output_folder, file_name), 'w') as f:
+        #     json.dump(data, f)
 
 
-def pred_lang_sentence(sentence: str):
-    """
-    still not 100% solution
-    """
-    result = detector.detect_multiple_languages_of(sentence)
-    if len(result) == 1:
-        return 'eng' if result[0].language == Language.ENGLISH else 'es'
-    else:
-        return 'mix'
-
-
-def has_switch(lst):
-    # for key, iter in itertools.groupby(lst):
-    #     print((key, len(list(iter))))
-    seq_th = 2
-    is_valid = {1: False, 0: False}
-    for key, iter in itertools.groupby(lst):
-        count = len(list(iter))
-        if count >= seq_th:
-            is_valid[key] = True
-    return all(list(is_valid.values()))
-
-
-#0.71 th -> 29 mixed, all are valid!
-#0.7 th -> 45 mixed, FP mistakes: eng:16
-
-def custom_pred_lang_sentence(sentence: str):
-    """
-    uses word-lvl predication and goes through the sequence
-    """
-
-    sentence = sentence.lower().strip()
-    tokens = sentence.split(' ')
-
-    english_words = ['hello', 'parrot', 'no', 'bomb', 'stones', 'telescope', 'human', 'car', 'tent']
-    if len(tokens) == 1:
-        for end_symbol in ['?', '.', '!', ',']:
-            if sentence.endswith(end_symbol):
-                sentence = sentence[:len(sentence)-1]
-        if sentence in english_words:
-            return 'eng'
-        if sentence in ['.', 'jirafa', 'ya', ':(']:
-            return 'none'
-        if len(sentence) == 0:
-            return 'none'
-
-    if len(tokens) == 2 and tokens[0] == 'a':
-        if tokens[1] in english_words:
-            return 'eng'
-
-
-    preds = [pred_lang_token(token) for token in sentence.split(' ')]
-    conf_preds = list(filter(lambda x: x[0] > 0.71, preds))
-    if len(conf_preds) == 0:
-        langs = ['eng' if p[1] == Language.ENGLISH else 'es' for p in preds]
-        return max(set(langs), key=langs.count)
-
-    langs = ['eng' if p[1] == Language.ENGLISH else 'es' for p in conf_preds]
-    binary_langs = [1 if l == 'eng' else 0 for l in langs]
-    if has_switch(binary_langs):
-        return 'mix'
-
-    _, counts = np.unique(langs, return_counts=True)
-    if len(counts) > 1 and counts[0] == counts[1]:
-        return pred_lang_sentence(sentence)
-
-    return max(set(langs), key=langs.count)
-
-def examples():
-    # bad word-lvl cls example. good whole sentence clf
-    print_deubg('go and make a u-turn')
-    # good word-lvl cls example. bad whole sentence clf
-    print_deubg('hi there! que comience el juego!!')
-    print_deubg('si debe empezar at the tent')
-    print_deubg('hola, q tal? ready to go?')
-    print_deubg('hello')
-    # other solution - sentence lvl
+def show_examples():
     sents = [
         'go down al la isla con el snake y ya llegastes',
         'donde empizo',
         'human',
         'car',
         'a car',
-        'hi there! que comience el juego!!', # correct
+        'hi there! que comience el juego!!',  # correct
         'go and make a u-turn',
         'hey, q tal? empezamos el juego?',
         'hi there, empezamos?',
-        'hola, q tal? ready to go?', # mistake
-        'si debe empezar at the tent', # mistake here
+        'hola, q tal? ready to go?',  # mistake
+        'si debe empezar at the tent',  # mistake here
         'ok',
         'si',
         'parrot',
-        'hello'
+        'hello',
+        'genial, he llegado a la isla con el tesoro',
+        'debe empezar',
     ]
-    print('sentence-lvl predication using multiple lang util')
+
+    print('token-lvl predication using lingua')
     for sentence in sents:
-        print(sentence, '->', pred_lang_sentence(sentence).upper())
+        print(sentence, '->', pred_sentence_via_token_lvl(sentence, pred_token_lingua).upper())
     print()
 
-
-    # sequence solution
-    print('sentence-lvl predication using Sequence counting and edge cases')
+    print('token-lvl predication using langid')
     for sentence in sents:
-        print(sentence, '->', custom_pred_lang_sentence(sentence).upper())
+        print(sentence, '->', pred_sentence_via_token_lvl(sentence, pred_token_langid).upper())
     print()
 
-    # print(has_switch([1, 1, 1, 1, 0, 1, 1]))
-    # print(has_switch([1,1,1,1,1,1,1]))
-    # print(has_switch([1,1,1,0,1,1,1]))
-    # print(has_switch([1,1,1,0,0,1,1]))
-    # print(has_switch([1,1,0,0,0,0,0]))
-    # print(has_switch([1,0,0,0,0,0,0]))
-    # print(has_switch([0,0,0,0,0,1]))
-    # print(has_switch([0,0,0,0,0,1,1,0,0,0]))
-
-
-def examples2():
-    sents = ['parrot',
-             'hello']
+    print('sentence-lvl predication using lingua')
     for sentence in sents:
-        print(sentence, '->', custom_pred_lang_sentence(sentence).upper())
+        print(sentence, '->', pred_sentence_lingua(sentence).upper())
     print()
+
+    print('sentence-lvl predication using lince-bert')
+    for sentence in sents:
+        print(sentence, '->', pred_sentence_bert(sentence).upper())
+    print()
+
 
 if __name__ == '__main__':
     languages = [Language.ENGLISH, Language.SPANISH]
-    detector = LanguageDetectorBuilder.from_languages(*languages).build()
+    lingua_detector = LanguageDetectorBuilder.from_languages(*languages).build()
 
+    langid.set_languages(['en', 'es'])
 
-    examples()
+    lid = LanguageIdentification('spa-eng')
+
+    show_examples()
+
     # run_on_dataset()
-    print(global_cs_counter)
